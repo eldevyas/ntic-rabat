3c9ffcdbcbd5d4acadc707266370a1424553ff4f
Yassine Atik <brucexx02@gmail.com>
2022-12-31 14:29:01 +0100

    Changed button styles

--- app/components/layout/header/header.tsx
+++ app/components/layout/header/header.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 
 export default function Header() {
+    const [selected, setSelected] = useState(0);
+    const buttons = [
+        {
+            name: 'Accueil',
+            component: <h1>Acceuil</h1>
+        },
+        {
+            name: 'Emplois',
+            component: <h1>Emplois</h1>
+        },
+        {
+            name: 'Forum',
+            component: <h1>Forum</h1>
+        },
+        {
+            name: 'Contact',
+            component: <h1>Contact</h1>
+        },
+    ]
     return (
         <>
             {/* Tailwind header, logo Image, 4 middle button links, and 2 buttons in the end */}
@@ -16,7 +35,7 @@ export default function Header() {
                     />
                 </div>
                 <div className="flex flex-row items-center justify-center gap-3">
-                    <button className="bg-[color:var(--C1)]	 bg-opacity-20 hover:bg-[color:var(--C5)] text-slate-100 font-bold hover:text-white py-2 px-4 rounded">
+                    {/* <button className="bg-[color:var(--C1)]	 bg-opacity-20 hover:bg-[color:var(--C5)] text-slate-100 font-bold hover:text-white py-2 px-4 rounded">
                         Accueil
                     </button>
 
@@ -30,7 +49,11 @@ export default function Header() {
 
                     <button className="bg-slate-400	 bg-opacity-20 hover:bg-slate-400 text-slate-500 font-bold hover:text-white py-2 px-4 rounded">
                         Contact
-                    </button>
+                    </button> */}
+
+                    {buttons.map((button, index) => {
+                        return <button className={selected == index ? 'bg-[color:var(--C1)]	 bg-opacity-20 hover:bg-[color:var(--C5)] text-slate-100 font-bold hover:text-white py-2 px-4 rounded' : 'bg-slate-400	 bg-opacity-20 hover:bg-slate-400 text-slate-500 font-bold hover:text-white py-2 px-4 rounded'} onClick={() => setSelected(index)}>{button.name}</button>;
+                    })}
                 </div>
             </header>
         </>
