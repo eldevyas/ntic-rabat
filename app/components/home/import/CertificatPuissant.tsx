a1e1643764b75019df28d84294852b5ee88682b9
Yassine Chettouch <95131504+yassine-ct@users.noreply.github.com>
2022-12-31 23:20:18 +0100

    Added A new Section Added a new Section

--- app/components/home/import/CertificatPuissant.tsx
+++ app/components/home/import/CertificatPuissant.tsx
@@ -1,10 +1,40 @@
 import React from "react";
+import { DefaultButton, IconButton } from "../../core/button";
+import ExpandCircleDownOutlinedIcon from "@mui/icons-material/ExpandCircleDownOutlined";
 import Image from "next/image";
 const CertificatPuissant = () => {
     return (
         <div className="CertificatPuissant">
-            <div className="Image">
-                <Image src="/assets/img/Table.png" width={100} height={100} />
+            <div className="Text">
+                <div className="Title">
+                    <h3>Certificat Puissant</h3>
+                    <h2>
+                        Un taux d&apos;insertion élevé dans le monde
+                        professionnel
+                    </h2>
+                </div>
+                <p>
+                    L&apos;un des facteurs de succés de notre établissement est
+                    la facilité d&apos;insertion des lauréats dans
+                    l&apos;entreprise. Ceci est rendu possible à travers:
+                </p>
+                <DefaultButton bgColor="Green">
+                    Formation altérnée
+                </DefaultButton>
+                <DefaultButton bgColor="Green">
+                    Stage de fin d'études
+                </DefaultButton>
+                <DefaultButton bgColor="Green">
+                    Partenariats avec les professionnels
+                </DefaultButton>
+            </div>
+            <div className="ImageContainer">
+                <Image
+                    alt=""
+                    src="/assets/img/Workspace.png"
+                    layout="fill"
+                    className="Image"
+                />
             </div>
         </div>
     );
