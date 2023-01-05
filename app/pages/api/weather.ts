a4217638fd8c12cbb152a12aba94d7ca8f4608d9
Yassine Chettouch <95131504+yassine-ct@users.noreply.github.com>
2023-01-05 17:24:51 +0100

    🌤️ Added Weather API to  the project

--- app/pages/api/weather.ts
+++ app/pages/api/weather.ts
@@ -1,33 +1,41 @@
-
-import React, { useState } from 'react'
+import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
-const Weather = (props: any) => {
-    // function setWeather(data: any) {
-    //     const weather = data;
-    // }
-    // console.log(props.data)
-    // var weather: {} = [];
-    // function setWeather(data: any) {
-    //     weather = data;
-    // }
-    // const [weather, setWeather] = useState({})
-    const [weather, setWeather] = useState({})
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+    // API  Data
     const apiKey = "f8e21ece8f005c43bba568195d4bd3e3";
-
     const rabat_lon = "-6.849131222272945"
-
     const rabat_lat = "33.97583368254488"
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${rabat_lat}&lon=${rabat_lon}&exclude=hourly,alerts&appid=${apiKey}&units=metric`;
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${rabat_lat}&lon=${rabat_lon}&appid=${apiKey}&units=matric`;
+    // Get the data from the API
+    const { data } = await axios.get(url);
 
-    // export response from api url
-    axios.get(url).then((response) => {
-        setWeather(response.data)
-    }
-    )
-    console.log(weather)
-    // TypeError: Cannot read properties of null (reading 'useState')
+    // Filter returned data to only take celcius degrees,description, icon and weather for the week
+
+
+
+    let filteredData = data.list.map((item: any) => {
+        if (item.dt_txt.includes('12:00:00')){
+            return {
+                icon: item.weather[0].icon,
+                temperature: item.main.temp,
+                humidity: item.main.humidity,
+                description: item.weather.description,
+            };
+        }
+        return;
+    });
+
+    // remove null values
+    filteredData = filteredData.filter((item: any) => {
+        return item!== null;
+    });
+
+    // res.status(200).json(data);
+    res.status(200).json(filteredData);
 
 }
-
-export default Weather
