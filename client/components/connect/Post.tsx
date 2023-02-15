935526c7710912866dadbc1b4c21be75b3db4c7a
Yassine Chettouch <95131504+yassine-ct@users.noreply.github.com>
2023-02-15 22:28:37 +0100

    Co-authored-by: Yassine Atik <yassineatik@users.noreply.github.com>

--- client/components/connect/Post.tsx
+++ client/components/connect/Post.tsx
@@ -3,16 +3,22 @@ import Image from "next/image";
 import { DefaultButton } from "../../components/core/button";
 import { formatDistanceToNow } from "date-fns";
 import axios from "axios";
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import * as Display from "../../services/displayAlert";
 
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteIcon from "@mui/icons-material/Favorite";
 const Post = (props: any) => {
+    const [Likes, setLikes] = useState(props.post.likes);
+    const [LikesCount, setLikesCount] = useState(props?.post?.likes?.length);
+    const [Comments, setComments] = useState(props.post.comments);
+    const [userHasLiked, setUserHaLiked] = useState(
+        Likes.find((like: any) => like.user_id === user?.id)
+    );
+
     const user = props.user;
     const created_at = props.post.created_at;
     const poster = props.post.user;
     const post = props.post;
-    const comments = props.post.comments;
-    const likes = props.post.likes;
     let timeAgo = formatDistanceToNow(new Date(created_at), {
         addSuffix: false,
     });
@@ -23,15 +29,19 @@ const Post = (props: any) => {
     timeAgo = timeAgo.replace("days", "j");
     timeAgo = timeAgo.replace("months", "mois");
 
-
     const likePost = () => {
         // get server url from env
         const url = process.env.SERVER_PUBLIC_API_URL;
+        if (Likes.find((like: any) => like.user_id === user?.id)) {
+            setLikesCount(LikesCount + 1);
+        } else {
+            setLikesCount(LikesCount - 1);
+        }
+        setUserHaLiked(!userHasLiked);
         axios
             .post(
                 url + `/post/${post.id}/like`,
-                {
-                },
+                {},
                 {
                     headers: {
                         "Content-Type": "application/json",
@@ -41,15 +51,14 @@ const Post = (props: any) => {
             )
             .then((res) => {
                 console.log(res.data);
-            }
-            )
+            })
             .catch((err) => {
                 console.log(err.response.data);
-            }
-            );
+                setUserHaLiked(!userHasLiked);
+                Display.pushFailure("Une erreur s'est survenue.");
+            });
     };
     // check if the user has liked the post
-
 
     return (
         <div className="Post">
@@ -70,17 +79,15 @@ const Post = (props: any) => {
             </div>
             <div className="PostActions">
                 {/* check if the user has already liked this post */}
-                {
-                    likes.find((like: any) => like.user_id === user?.id) ? (
-                        <DefaultButton onClick={likePost} className="Liked">
-                            {likes.length} <FavoriteIcon />
-                        </DefaultButton>
-                    ) : (
-                        <DefaultButton onClick={likePost} className="Like">
-                            {likes.length} <FavoriteBorderIcon />
-                        </DefaultButton>
-                    )
-                }
+                {userHasLiked ? (
+                    <DefaultButton onClick={likePost} className="Liked">
+                        {LikesCount} <FavoriteIcon />
+                    </DefaultButton>
+                ) : (
+                    <DefaultButton onClick={likePost} className="Like">
+                        {LikesCount} <FavoriteBorderIcon />
+                    </DefaultButton>
+                )}
                 <DefaultButton>Comment</DefaultButton>
                 <DefaultButton>Save</DefaultButton>
             </div>
@@ -88,4 +95,12 @@ const Post = (props: any) => {
     );
 };
 
+// cc bro
+// what did u change?
+// useState for the button of likes
+// fach kattbdel , chno kytbdel ? ana dayr lik follow sir liha
+// Dynamic
+//
+// waaa narriiii , sf chftha , 3wr dinmi
+// HHHHHHHHHHHHHH
 export default Post;
